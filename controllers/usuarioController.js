f1a37ab9fef23cd24eda7604fe31ca2626b6769d
juanesgv <juanesgarviedma23@gmail.com>
2024-02-10 14:20:00 -0500

    generación de un json web token

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,6 @@
 import Usuario from "../model/Usuario.js"
 import generarId from "../helpers/generarId.js"
+import generarJWT from "../helpers/generarJWT.js"
 
 const registrarUsuario = async (req, res) => {
     //Evitar registro duplicados
@@ -46,6 +47,7 @@ const autenticar = async (req,res) => {
             _id: usuario._id,
             nombre: usuario.nombre,
             email: usuario.email,
+            token: generarJWT(usuario._id)
         })
     }else{
         const error = new Error('La contraseña es incorrecta')
