cc49ac4e82843bbe12e969fcb17a65d2dd660f0e
juanesgv <juanesgarviedma23@gmail.com>
2024-02-12 17:40:18 -0500

    Creación de los métodos de editarProyecto y eliminarProyecto

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -35,16 +35,66 @@ const obtenerProyecto = async (req, res) => {
       return res.status(401).json({ msg: error.message });
     }
     res.json(proyecto);
-
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ msg: "Error interno del servidor", error: error.message });
+    return res
+      .status(500)
+      .json({ msg: "Error interno del servidor", error: error.message });
   }
 };
 
-const editarProyecto = async (req, res) => {};
+const editarProyecto = async (req, res) => {
+  const { id } = req.params;
+  const proyecto = await Proyecto.findById(id);
 
-const eliminarProyecto = async (req, res) => {};
+  //verifica si el proyecto existe
+  if (!proyecto) {
+    const error = new Error("No se encontró el proyecto");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  //verifica si el usuario autenticado es el creador del proyecto
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(401).json({ msg: error.message });
+  }
+
+  proyecto.nombre = req.body.nombre || proyecto.nombre;
+  proyecto.descripcion = req.body.descripcion || proyecto.descripcion;
+  proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega;
+  proyecto.cliente = req.body.cliente || proyecto.cliente;
+
+  try {
+    const proyectoAlmacenado = await proyecto.save();
+    return res.json(proyectoAlmacenado);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const eliminarProyecto = async (req, res) => {
+  const { id } = req.params;
+  const proyecto = await Proyecto.findById(id);
+
+  //verifica si el proyecto existe
+  if (!proyecto) {
+    const error = new Error("No se encontró el proyecto");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  //verifica si el usuario autenticado es el creador del proyecto
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(401).json({ msg: error.message });
+  }
+
+  try {
+    await proyecto.deleteOne()
+    res.json({msg:"Proyecto eliminado existosamente"})
+  } catch (error) {
+    console.log(error)
+  }
+};
 
 const agregarColaborador = async (req, res) => {};
 
