af3a273fd7863951e6802be4a7d4fc64cf957e01
juanesgv <juanesgarviedma23@gmail.com>
2024-02-22 17:39:31 -0500

    introducción a socket.io

--- index.js
+++ index.js
@@ -38,8 +38,30 @@ app.use('/api/tareas', tareaRoutes)
 
 const PORT = process.env.PORT || 4000;
 
-
-
-app.listen(PORT, () => {
+const servidor = app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`)
+})
+
+//socket.io
+import { Server } from 'socket.io';
+
+const io = new Server(servidor, {
+    pingTimeout : 60000,
+    cors : {
+        origin: process.env.FRONTEND_URL,
+    },
+})
+
+io.on('connection', (socket)  =>{
+    console.log("Conectado a Socker.io")
+
+    //definir los eventos de socket.io
+
+    //recibimos datos desde react con el evento 'prueba'
+    socket.on('prueba', (proyectos)=>{
+        console.log('Prueba desde socket.io', proyectos)
+    })
+
+    //enviar datos a react
+    socket.emit('respuesta', {nombre: 'Juan' , apellido : 'Garcia'})
 })
